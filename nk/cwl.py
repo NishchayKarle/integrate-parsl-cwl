ed6577dc2dd43b8e371ecd0a7b80d6ec811b29e6
nishchaykarle <nishchaykarle@gmail.com>
2024-01-28 23:27:03 -0600

    update

--- nk/cwl.py
+++ nk/cwl.py
@@ -1,26 +1,35 @@
 import yaml
 import os
 import subprocess
-import ruamel.yaml
 from typing import Dict, Any
 import pprint
 import copy
 
 
 class CommandLineTool:
-    def __init__(self, commandlinetool_cwl):
-        self.baseCommand = commandlinetool_cwl["baseCommand"]
+    def __init__(self, cwl_file: str):
+        with open(cwl_file, "r") as cwl_file:
+            cwl = yaml.safe_load(cwl_file)
+        
+        if not self.__validate_cwl(cwl):
+            raise Exception("Invalid CommandLineTool CWL file")
+
+        self.baseCommand = cwl["baseCommand"]
         self.inputs = []
         self.outputs = []
 
-        self.__set_inputs(commandlinetool_cwl["inputs"])
-        self.__set_outputs(commandlinetool_cwl["outputs"])
+        self.__set_inputs(cwl["inputs"])
+        self.__set_outputs(cwl["outputs"])
 
     def __repr__(self) -> str:
         return pprint.pformat([self.baseCommand, self.inputs, self.outputs])
 
     def __str__(self) -> str:
         return pprint.pformat([self.baseCommand, self.inputs, self.outputs])
+    
+    def __validate_cwl(self, cwl_content: Dict[str, Any]) -> bool:
+        # TODO: IMPLEMENT ME!
+        return True
 
     def __set_inputs(self, cwl_inputs):
         if isinstance(cwl_inputs, list):
@@ -39,9 +48,33 @@ class CommandLineTool:
         self.__set_outputs = cwl_outputs
 
     @property
-    def command(self) -> str:
-        return f"{self.baseCommand} " + " ".join(
+    def command_template(self) -> str:
+        return f"COMMAND: \n{self.baseCommand} " + " ".join(
             f'<{inpt["id"]}>' for inpt in self.inputs
+        )
+
+    def get_command(self, **kwargs):
+        # handle case where kwargs has unnecessary args
+        cmd = f"{self.baseCommand}"
+        args = []
+
+        for inpt in self.inputs:
+            if inpt["id"] in kwargs:
+                args.append((kwargs[inpt["id"]], inpt["type"]))
+
+            elif "default" in inpt:
+                args.append((inpt["default"], inpt["type"]))
+
+            else:
+                print(f"ERROR: missing input parameter {inpt['id']}")
+
+        return (
+            cmd
+            + " "
+            + " ".join(
+                f'"{arg}"' if arg_type == "string" else f"{arg}"
+                for arg, arg_type in args
+            )
         )
 
     def run(self, **kwargs):
@@ -50,8 +83,12 @@ class CommandLineTool:
             capture_output=True,
             text=True,
         )
-        print(f"{result.stdout=}")
-        print(f"{result.stderr=}")
+        if not result.stderr:
+            print(f"STDOUT: \n{result.stdout}")
+
+        else:
+            print(f"STDERR: \n{result.stderr}")
+
 
 class Workflow:
     def __init__(self, cwl: Dict[str, Any]) -> None:
@@ -59,11 +96,11 @@ class Workflow:
         self.inputs = []
         self.outputs = []
         self.steps = []
-        
+
         self.__extract_commands_from_cwl(cwl)
-        self.__set_inputs(cwl['inputs'])
-        self.__set_outputs(cwl['outputs'])
-    
+        self.__set_inputs(cwl["inputs"])
+        self.__set_outputs(cwl["outputs"])
+
     def __set_inputs(self, cwl_inputs):
         if isinstance(cwl_inputs, list):
             self.inputs = copy.deepcopy(cwl_inputs)
@@ -79,29 +116,29 @@ class Workflow:
 
     def __set_outputs(self, cwl_outputs):
         self.__set_outputs = cwl_outputs
-    
+
 
 def extract_commands_from_cwl(cwl, path):
     cmds = []
-    
-    with open(os.path.join(path, cwl), 'r') as cwl_file:
+
+    with open(os.path.join(path, cwl), "r") as cwl_file:
         cwl_content = yaml.safe_load(cwl_file)
 
         # Check if CWL file is a CommandLineTool
-        if 'class' in cwl_content and cwl_content['class'] == 'CommandLineTool':
+        if "class" in cwl_content and cwl_content["class"] == "CommandLineTool":
             cmdlinetool = CommandLineTool(cwl_content)
             return [cmdlinetool]
 
         # Check if CWL file is a Workflow
-        elif 'class' in cwl_content and cwl_content['class'] == 'Workflow':
+        elif "class" in cwl_content and cwl_content["class"] == "Workflow":
             # Extract the base command from the first step
-            for step, todo in cwl_content.get('steps', {}).items():
+            for step, todo in cwl_content.get("steps", {}).items():
                 if step:
-                    run_cwl = todo.get('run', '')
+                    run_cwl = todo.get("run", "")
                     if isinstance(run_cwl, str):
                         cmdlinetool = extract_commands_from_cwl(run_cwl, path)
                     cmds.extend(cmdlinetool)
         else:
-            raise Exception('ERROR')
-    
+            raise Exception("ERROR")
+
     return cmds
